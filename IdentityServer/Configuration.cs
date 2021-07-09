c40c6be7e940042c484c9ac3b7fada6654da08d8
Odis <tracemode@mail.ru>
2021-07-09 14:25:45 +0300

    Configuration changed

--- IdentityServer/Configuration.cs
+++ IdentityServer/Configuration.cs
@@ -1,5 +1,4 @@
-﻿using IdentityServer.Profiles;
-using IdentityServer4;
+﻿using IdentityServer4;
 using IdentityServer4.Models;
 using System.Collections.Generic;
 
@@ -8,7 +7,7 @@ namespace IdentityServer
     public static class Configuration
     {
         public static IEnumerable<ApiScope> ApiScopes =>
-            new List<ApiScope> 
+            new List<ApiScope>
             {
                 new ApiScope("InventoryAPI", "InventoryApp Web API"),
                 new ApiScope("CheckerAPI", "CheckerApp Web API")
@@ -18,20 +17,16 @@ namespace IdentityServer
             new List<IdentityResource>
             {
                 new IdentityResources.OpenId(),
-                new InventoryAppProfile()
+                new IdentityResources.Profile(),
+                new IdentityResource("inventoryapp_role", new[] { "inventoryapp_role" }),
+                new IdentityResource("checkerapp_role", new[] { "checkerapp_role" }),
             };
 
         public static IEnumerable<ApiResource> ApiResources =>
             new List<ApiResource>
             {
-                new ApiResource("InventoryAPI", "InventoryApp Web API", new []
-                { 
-                    "inventoryAppRole"
-                }),
-                new ApiResource("CheckerAPI", "CheckerApp Web API", new[]
-                {
-                    "checkerAppRole"
-                })
+                new ApiResource("InventoryAPI", "InventoryApp Web API"),
+                new ApiResource("CheckerAPI", "CheckerApp Web API")
             };
 
         public static IEnumerable<Client> Clients =>
@@ -64,9 +59,42 @@ namespace IdentityServer
                     {
                         IdentityServerConstants.StandardScopes.OpenId,
                         IdentityServerConstants.StandardScopes.Profile,
+                        "inventoryapp_role",
                         "InventoryAPI"
                     },
-                    AllowAccessTokensViaBrowser = true
+                    AllowAccessTokensViaBrowser = true,
+                },
+                new Client
+                {
+                    ClientId = "checker-web-api",
+                    ClientName = "CheckerApp Web",
+                    AllowedGrantTypes = GrantTypes.Code,
+                    RequirePkce = true,
+                    RequireClientSecret = false,
+                    RequireConsent = false,
+                    RedirectUris =
+                    {
+                        "http://localhost:6000/authentication/login-callback",
+                        "https://localhost:6001/authentication/login-callback"
+                    },
+                    AllowedCorsOrigins =
+                    {
+                        "http://localhost:6000",
+                        "https://localhost:6001"
+                    },
+                    PostLogoutRedirectUris =
+                    {
+                        "http://localhost:6000/authentication/logout-callback",
+                        "https://localhost:6001/authentication/logout-callback"
+                    },
+                    AllowedScopes =
+                    {
+                        IdentityServerConstants.StandardScopes.OpenId,
+                        IdentityServerConstants.StandardScopes.Profile,
+                        "checkerapp_role",
+                        "CheckerAPI"
+                    },
+                    AllowAccessTokensViaBrowser = true,
                 }
             };
     }
